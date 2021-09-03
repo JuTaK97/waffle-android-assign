fdecaf6e6a60c4cd243b95c39632ab475e95ef9f
JuTaK97 <didwngus01@snu.ac.kr>
2021-09-03 23:49:40 +0900

    최종

--- app/src/main/java/com/example/assignment0/IntroduceActivity.kt
+++ app/src/main/java/com/example/assignment0/IntroduceActivity.kt
@@ -2,6 +2,9 @@ package com.example.assignment0
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.BitmapFactory
+import android.widget.ImageView
+
 
 class IntroduceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/assignment0/KotlinPracticeActivity.kt
+++ app/src/main/java/com/example/assignment0/KotlinPracticeActivity.kt
@@ -9,6 +9,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.lang.NumberFormatException
 import java.lang.StringBuilder
 
 class KotlinPracticeActivity : AppCompatActivity() {
@@ -22,21 +24,40 @@ class KotlinPracticeActivity : AppCompatActivity() {
     }
 
     private fun enterNumbers(view : View) {
-        val input = findViewById<EditText>(R.id.edit_text)
-        val splitted = input.text.split(" ")
+        val edit_text = findViewById<EditText>(R.id.edit_text)
 
+        //no inputs
+        if(edit_text.text.isEmpty()) {
+            Toast.makeText(this, getString(R.string.string_emptyInput), Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val splitted = edit_text.text.split(" ")
         val nums = arrayListOf<Int>()
-
         val a = StringBuilder()
         val b = StringBuilder()
         val c = StringBuilder()
 
 
-        for(i in 0..splitted.size-1){
+        for(i in splitted.indices){
+
+            //exceptions
+            try {
+                splitted[i].toInt()
+            } catch (e : NumberFormatException) {
+                Toast.makeText(this, getString(R.string.string_errorInput), Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            //build result A
             a.append(splitted[i]+"명 ")
+
+            //build result B
             if(splitted[i].toInt()%2==0) {
                 b.append(splitted[i]+" ")
             }
+
+            //prepare for result C
             nums.add(splitted[i].toInt())
         }
 
@@ -51,19 +72,21 @@ class KotlinPracticeActivity : AppCompatActivity() {
             }
         }
 
+        //build result C
         for(i in 0..nums.size-1) {
             c.append(nums[i].toString()+" ")
         }
 
+        //build total output
         val total = StringBuilder()
-        total.append(a.toString()+"\n"+b.toString()+"\n"+c.toString())
-        findViewById<TextView>(R.id.resultText).text = total
+        total.append(a.toString()+"\n")
+        total.append( (if(b.isEmpty()) "짝수 없음" else b.toString())+"\n")
+        total.append(c.toString())
+        findViewById<TextView>(R.id.result_text).text = total
 
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-
-
     }
 
 }
